f3e235a4931d4eff3e0f1b1d6b513a08da870ff6
Lenovo <kvtanna.om@gmail.com>
2020-11-05 09:53:13 +0530

    Updates

---
+++ precache-manifest.24f68db90b75cab3ff641ff7e2b6a11c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f514cde2ed5325af9d52cb8b30512aff",
+    "url": "/food-frontend/index.html"
+  },
+  {
+    "revision": "8214a69d837886c8c5d4",
+    "url": "/food-frontend/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "2af186820c27e9e07212",
+    "url": "/food-frontend/static/css/main.3ec70635.chunk.css"
+  },
+  {
+    "revision": "8214a69d837886c8c5d4",
+    "url": "/food-frontend/static/js/2.eaebf6a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/food-frontend/static/js/2.eaebf6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af186820c27e9e07212",
+    "url": "/food-frontend/static/js/main.d11bc6e1.chunk.js"
+  },
+  {
+    "revision": "956d69962f1380219276",
+    "url": "/food-frontend/static/js/runtime-main.b72bebee.js"
+  }
+]);
