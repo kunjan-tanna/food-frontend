60cdb885852b02bd1fb5681d4f029f53cceec817
Lenovo <kvtanna.om@gmail.com>
2020-11-24 21:19:42 +0530

    Updates

---
+++ precache-manifest.f8f6222d8c22f8fa38626dc28b482f76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0a7bf894ea16d70cdc4fe41f06c730",
+    "url": "/food-frontend/index.html"
+  },
+  {
+    "revision": "d79a708d4ae9d648281b",
+    "url": "/food-frontend/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "9feae592698b81e18b44",
+    "url": "/food-frontend/static/css/main.3ec70635.chunk.css"
+  },
+  {
+    "revision": "d79a708d4ae9d648281b",
+    "url": "/food-frontend/static/js/2.e64377f3.chunk.js"
+  },
+  {
+    "revision": "7d2579c8bc39cbb93e4298ffb99d34c7",
+    "url": "/food-frontend/static/js/2.e64377f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9feae592698b81e18b44",
+    "url": "/food-frontend/static/js/main.0a4a466d.chunk.js"
+  },
+  {
+    "revision": "956d69962f1380219276",
+    "url": "/food-frontend/static/js/runtime-main.b72bebee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-frontend/precache-manifest.d4bf44f2e662d8a8524d220669624125.js"
+  "/food-frontend/precache-manifest.f8f6222d8c22f8fa38626dc28b482f76.js"
 );
 
 self.addEventListener('message', (event) => {
